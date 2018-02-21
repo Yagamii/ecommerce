7ceb54939d13b6556b6fc32c66ffd2dc7872445a
Yagamii <yagogomesc@gmail.com>
2018-02-21 15:48:43 -0300

    Criando PageAdmin e tpl_admin

--- vendor/Yagamii/php-classes/src/Page.php
+++ vendor/Yagamii/php-classes/src/Page.php
@@ -12,12 +12,12 @@
         "data"=>[]
     ];
 
-    public function __construct($opt = array()){
+    public function __construct($opt = array(), $tpl_dir = "/views/"){
 
       $this->options = array_merge($this->defaults, $opt);
 
         $config = array(
-    					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+    					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
     					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
     					"debug"         => false // set to false to improve the speed
     				   );
