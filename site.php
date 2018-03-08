a0d277be394c9b041db52802bcba4e240484c81f
Yagamii <yagogomesc@gmail.com>
2018-03-08 17:20:11 -0300

    Carrinho - Calculo de frete

--- site.php
+++ site.php
@@ -69,9 +69,11 @@ $app->get("/cart", function(){
 
   $page = new Page();
 
+
   $page->setTpl("cart", [
     'cart'=>$cart->getValues(),
-    'products'=>$cart->getProducts()
+    'products'=>$cart->getProducts(),
+    'error'=>Cart::getMsgError()
   ]);
 
 });
@@ -122,5 +124,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
     header("Location: /cart");
     exit;
+});
+
+$app->post("/cart/freight", function(){
+
+    $cart = Cart::getFromSession();
+
+    $cart->setFreight($_POST['zipcode']);
+
+    header("Location: /cart");
+    exit;
+
 });
  ?>
