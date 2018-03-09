519cf472e880ad78c034f65301a9abd68cca04a6
Yagamii <yagogomesc@gmail.com>
2018-03-09 15:31:11 -0300

    Site Cadastro

--- vendor/Yagamii/php-classes/src/Model/User.php
+++ vendor/Yagamii/php-classes/src/Model/User.php
@@ -118,7 +118,7 @@ class User extends Model {
 :inadmin)", array(
 		":desperson"=>$this->getdesperson(),
 		":deslogin"=>$this->getdeslogin(),
-		":despassword"=>$this->User::getPasswordHash(getdespassword()),
+		":despassword"=>User::getPasswordHash($this->getdespassword()),
 		":desemail"=>$this->getdesemail(),
 		":nrphone"=>$this->getnrphone(),
 		":inadmin"=>$this->getinadmin()
